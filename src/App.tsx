28689f968433d3b7467369dadea62b29faa4efa2
Venkata Raviteja Gullapudi <venkataravitejagullapudi@gmail.com>
2024-07-03 00:33:40 +0530

    feat: added nextui and related changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { Provider } from "react-redux";
 import { Outlet } from "react-router-dom";
+
 import AppLayout from "./layout/AppLayout";
-import applicationStore from "./store";
 
 function App() {
   return (
-    <Provider store={applicationStore}>
-      <AppLayout>
-        <Outlet />
-      </AppLayout>
-    </Provider>
+    <AppLayout>
+      <Outlet />
+    </AppLayout>
   );
 }
 
