5f4bc1a955b1f7c2f82ad037f231e596cf571301
harshit120 <76102441+harshit120@users.noreply.github.com>
2024-10-04 14:08:03 +0530

    Update and rename question-component.jsx to question-component.tsx

--- src/components/question-component.tsx
+++ src/components/question-component.tsx
@@ -1,34 +1,40 @@
 import checkOne from "../assets/checkOne.png";
 import closeOne from "../assets/closeOne.png";
 import { AppTexts } from "../lib/utils/texts";
+import { Question } from "../types/index";
 
-function QuestionComponent({ values, index }) {
-  const { question, options, correctAnswers } = values;
+type Props = {
+  question: Question;
+  index: number;
+};
+
+function QuestionComponent({ question: questionData, index }: Props) {
+  const { question, options, answer } = questionData;
 
   return (
     <div className="flex flex-row gap-2 break-words">
-      <p className={`ml-${index <= 9 ? "2" : "0"}`}>{index}.</p>
+      <p className={`ml-${index < 9 ? "2" : "0"}`}>{index + 1}.</p>
       <div className="flex flex-col w-full space-y-2">
-        <p className="break-words">{question}</p>
+        <p>{question}</p>
         <div className="grid grid-cols-12 gap-3">
-          {options.map((option, i) => (
+          {options.map((val, i) => (
             <div
-              key={i}
-              className={`col-span-full min-[481px]:col-span-6 break-words`}
+              key={val.id}
+              className={`col-span-full min-[481px]:col-span-6`}
             >
               <div
                 className={`flex flex-row items-start space-x-2 ${i % 2 === 1 ? "min-[481px]:ml-6" : "min-[481px]:ml-0"} ${i % 2 === 0 ? "min-[481px]:mr-6" : "min-[481px]:mr-0"}`}
               >
                 <img
                   alt={
-                    correctAnswers.includes(option)
+                    answer.optionIds.includes(val.id)
                       ? AppTexts.correctQuestion
-                      : AppTexts.wrongQuestion
+                      : AppTexts.incorrectQuestion
                   }
                   className="  mt-1"
-                  src={correctAnswers.includes(option) ? checkOne : closeOne}
+                  src={answer.optionIds.includes(val.id) ? checkOne : closeOne}
                 />
-                <p className="w-full">{option}</p>
+                <p className="w-full">{val.option}</p>
               </div>
             </div>
           ))}
