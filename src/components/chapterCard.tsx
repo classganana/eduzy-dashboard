57590fe0e4b5e24b8e49ae53d514e7af4a845edb
Venkata Raviteja Gullapudi <venkataravitejagullapudi@gmail.com>
2024-11-09 21:13:31 +0530

    feat: chapter card completed

--- src/components/chapterCard.tsx
+++ src/components/chapterCard.tsx
@@ -1,39 +1,68 @@
-import AppModal from "./app-modal-button";
-import QuestionComponent from "./question-component";
+import { Card, CardBody, CardHeader, Checkbox } from "@nextui-org/react";
+import { useState } from "react";
 
-import { AppTexts } from "@/lib/utils/texts";
+import { cn } from "@/lib/utils";
 import { Chapter } from "@/types";
 
 type Props = {
   chapter: Chapter;
+  isSelectable?: boolean; // New prop to determine if the card is selectable
+  onSelectionChange?: (selected: boolean) => void;
 };
 
-const ChapterCard = ({ chapter }: Props) => {
-  return (
-    <div>
-      Chapter Card
-      {chapter.name}
-      {chapter.description}
-      <AppModal
-        body={
-          <div className="flex flex-col gap-5">
-            {chapter.questions.map((question, index) => {
-              return (
-                <QuestionComponent
-                  key={chapter.id + question.id}
-                  index={index}
-                  question={question}
-                />
-              );
-            })}
-          </div>
-        }
-        header={AppTexts.previewQuestionsHeading}
-        triggerButton={{
-          label: AppTexts.preview,
-        }}
-      />
+const ChapterCard = ({
+  chapter,
+  isSelectable = true,
+  onSelectionChange,
+}: Props) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const toggleCheckbox = () => {
+    setIsSelected(!isSelected);
+    if (onSelectionChange) {
+      onSelectionChange(!isSelected);
+    }
+  };
+
+  const handleCheckboxChange = (selected: boolean) => {
+    setIsSelected(selected);
+  };
+
+  const chapterCardContent = (
+    <div className="w-full">
+      <CardHeader>{chapter.name || chapter.id}</CardHeader>
+      <CardBody>{chapter.description}</CardBody>
     </div>
+  );
+
+  return (
+    <Card
+      className={cn(
+        "w-full max-w-[20rem] h-[12rem] bg-content1",
+        "hover:bg-content2",
+        "rounded-lg gap-2 p-4 border-2 border-transparent",
+        { "cursor-pointer": isSelectable },
+        { "border-primary": isSelected && isSelectable },
+      )}
+      onClick={isSelectable ? toggleCheckbox : () => {}}
+      onKeyDown={isSelectable ? toggleCheckbox : () => {}}
+    >
+      {isSelectable ? (
+        <Checkbox
+          aria-label={chapter.name || chapter.id}
+          className="flex flex-row-reverse items-start"
+          classNames={{
+            base: "mr-2",
+          }}
+          isSelected={isSelected}
+          onValueChange={handleCheckboxChange}
+        >
+          {chapterCardContent}{" "}
+        </Checkbox>
+      ) : (
+        chapterCardContent
+      )}
+    </Card>
   );
 };
 
